55c131a57830d9c9c3d2ba83caffec0e3fbac7cc
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-22 20:10:00 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,12 @@ file_path = './lib/emoticons.yml'
 
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
+  get_meaning = {}
+  get_emoticon = {}
+  emoticons.each do |meaning, emoticon_pair|
+    
   binding.pry
+  end
 end
 
 def get_japanese_emoticon
