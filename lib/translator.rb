cc54ca1fbf51743e011297c98e5690aece8aa284
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-22 20:25:26 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,9 @@ def load_library(file_path)
   emoticon_library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(western_emoticon)
+  emoticon_library = load_library(file_path)
+  
 end
 
 def get_english_meaning
